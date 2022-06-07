22bd67d94e7704946564d5f7b03c4c1b74ee48c6
Jose Reyes <jreyesri@cbu.edu>
2022-06-07 11:36:53 -0400

    Can now fetch data using button

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let itunesUrlSearch = "https://itunes.apple.com/search?term="
+
 
 let itunesUrl = "https://itunes.apple.com/search?term=jack+johnson&limit=5"
 
@@ -24,45 +24,103 @@ fetch(itunesUrl, {
 
 function buildItunes(itunesData) {
 
-    // console.log(itunesData.results[0].trackName)
+    // // console.log(itunesData.results[0].trackName)
 
-    let itunesElement = document.createElement('div')
-    // profileElement.classList.add('topStyle')
+    // let itunesElement = document.createElement('div')
+    // // profileElement.classList.add('topStyle')
 
-    for (song of itunesData.results) {
-        console.log(song.trackName)
+    // for (song of itunesData.results) {
+    //     console.log(song.trackName)
 
-        let songElement = document.createElement('p')
-        songElement.innerText = `${song.trackName}`
-        itunesElement.appendChild(songElement)
+    //     let songElement = document.createElement('p')
+    //     songElement.innerText = `${song.trackName}`
+    //     itunesElement.appendChild(songElement)
 
-        // itunesInfo.appendChild(itunesElement)
-    }
+    //     // itunesInfo.appendChild(itunesElement)
+    // }
 
-    itunesInfo.appendChild(itunesElement)
+    // itunesInfo.appendChild(itunesElement)
 
-    // let nameElement = document.createElement('h1')
-    // // nameElement.classList.add('nameStyle')
-    // nameElement.innerText = `${itunesData.artistName}`
-    // itunesElement.appendChild(nameElement)
-
+    // // let nameElement = document.createElement('h1')
+    // // // nameElement.classList.add('nameStyle')
+    // // nameElement.innerText = `${itunesData.artistName}`
+    // // itunesElement.appendChild(nameElement)
 }
+
 
 let form = document.getElementById("form")
 // console.log(form)
 
+let submitButton = document.getElementById("submit")
 
 let searchVal = document.getElementById("search")
 
+let itunesUrlSearch = "https://itunes.apple.com/search?term="
 
-// form.addEventListener('submit', (event) => {
+submitButton.addEventListener('click', (event) => {
+    // console.log(searchVal.value)
+    let userInput = searchVal.value
+    //console.log(userInput)
+    let limitSearch = "&limit=5"
+    let userSearch = itunesUrlSearch + `${userInput}` + limitSearch
+    console.log(userSearch)
 
-// }
+
+    fetch(userSearch, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    })
+        .then(function (response) {
+            // the response is the promised data
+            return response.json()
+            // put the response in JSON format
+        })
+        .then(function (data) {
+            // data refers to what the above promise returned (response.json())
+            console.log("Response from GitHub API: ", data)
+            // console log the data
+            buildUser(data)
+
+        })
 
 
-form.addEventListener("click", (event) => {
-    console.log(searchVal.value)
+    function buildUser(itunesData) {
+        //console.log(itunesData.results[0].trackName)
+
+        let itunesElement = document.createElement('div')
+        // profileElement.classList.add('topStyle')
+
+        for (song of itunesData.results) {
+            console.log(song.trackName)
+
+            let songElement = document.createElement('p')
+            songElement.innerText = `${song.trackName}`
+            itunesElement.appendChild(songElement)
+
+            // itunesInfo.appendChild(itunesElement)
+        }
+
+        itunesInfo.appendChild(itunesElement)
+    }
+
 })
+
+
+
+// jack+johnson
+
+
+
+
+
+// form.addEventListener("click", (event) => {
+//     // console.log(searchVal.value)
+//     let userInput = searchVal.value
+//     // console.log(userInput)
+//     let userSearch = itunesUrlSearch + `${userInput}`
+//     console.log(userSearch)
+
+// })
 
 
 
@@ -72,11 +130,3 @@ form.addEventListener("click", (event) => {
 
 // })
 
-
-
-// let submit = () => {
-//     let values = ""
-//     values += document.getElementsByName("search")[0].value
-//     console.log(1)
-//     console.log(values)
-// }

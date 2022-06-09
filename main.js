2ca5674c39bbdf3ed6aebc885cd8e49b813c850f
Jose Reyes <jreyesri@cbu.edu>
2022-06-08 23:41:38 -0400

    Cleaned up the JS functions

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let itunesUrlSearch = "https://itunes.apple.com/search?term="
 // submitButton.addEventListener('click', (event) => {
 
 form.addEventListener('submit', (event) => {
-    
+
     event.preventDefault()
 
     itunesInfo.innerHTML = ""
@@ -40,84 +40,57 @@ form.addEventListener('submit', (event) => {
         })
         .then(function (data) {
             // data refers to what the above promise returned (response.json())
-            console.log("Response from GitHub API: ", data)
+            console.log("Response from itunes API: ", data)
             // console log the data
             buildUser(data)
 
         })
 
-
-    function buildUser(itunesData) {
-        //console.log(itunesData.results[0].trackName)
-
-        let itunesElement = document.createElement('div')
-        // profileElement.classList.add('topStyle')
-
-        for (item of itunesData.results) {
-            console.log(item.trackName)
-
-
-            let imageElement = document.createElement('img')
-            imageElement.src = item.artworkUrl100
-            imageElement.alt = 'Artwork of ' + `${item.artistName}`
-            itunesElement.appendChild(imageElement)
-
-
-            let songElement = document.createElement('p')
-            songElement.innerText = `${item.trackName}`
-            itunesElement.appendChild(songElement)
-
-
-            let artistElement = document.createElement('p')
-            artistElement.innerText = `${item.artistName}`
-            itunesElement.appendChild(artistElement)
-
-            // let audioTest = document.createElement("audio")
-            // audioTest.controls = true
-            // audioTest.src = item.previewUrl
-            // itunesElement.appendChild(audioTest)
-
-            imageElement.addEventListener("click", (event) => {
-                let playAudio = document.querySelector('audio')
-                console.log(item.trackName)
-                playAudio.id = `audio=player`
-                playAudio.controls = `controls`
-                playAudio.src = `${item.previewUrl}`
-                playAudio.type = `audio`
-            // itunesElement.appendChild(audioTest)
-
-
-            })
-
-        }
-
-        itunesInfo.appendChild(itunesElement)
-    }
-
 })
 
 
+function buildUser(itunesData) {
+    //console.log(itunesData.results[0].trackName)
 
-// jack+johnson
+    // let itunesElement = document.createElement('div')
+    // profileElement.classList.add('topStyle')
+
+    for (item of itunesData.results) {
+        console.log(item.trackName)
+
+        let itunesElement = document.createElement('div')
+
+        let imageElement = document.createElement('img')
+        imageElement.src = item.artworkUrl100
+        imageElement.alt = 'Artwork of ' + `${item.artistName}`
+        itunesElement.appendChild(imageElement)
 
 
+        let songElement = document.createElement('p')
+        songElement.innerText = `${item.trackName}`
+        itunesElement.appendChild(songElement)
 
 
+        let artistElement = document.createElement('p')
+        artistElement.innerText = `${item.artistName}`
+        itunesElement.appendChild(artistElement)
 
-// form.addEventListener("click", (event) => {
-//     // console.log(searchVal.value)
-//     let userInput = searchVal.value
-//     // console.log(userInput)
-//     let userSearch = itunesUrlSearch + `${userInput}`
-//     console.log(userSearch)
+        // let audioTest = document.createElement("audio")
+        // audioTest.controls = true
+        // audioTest.src = item.previewUrl
+        // itunesElement.appendChild(audioTest)
 
-// })
+        imageElement.addEventListener("click", (event) => {
+            let playAudio = document.querySelector('audio')
+            console.log(item.trackName)
+            playAudio.id = `audio=player`
+            playAudio.controls = `controls`
+            playAudio.src = `${item.previewUrl}`
+            playAudio.type = `audio`
+            // itunesElement.appendChild(audioTest)
+        })
 
-
-
-// form.addEventListener('submit', (event) => {
-
-     // event.preventDefault();
-
-// })
-
+        itunesInfo.appendChild(itunesElement)
+    }
+    
+}
